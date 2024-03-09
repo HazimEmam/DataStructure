05845b5789c188a88c62c58724018ee689a40a9c
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2024-03-09 19:29:42 +0200

    Task_2: Shell Sort Add

--- Assignment-1/Task-2/cmake-build-debug/cmake_install.cmake
+++ Assignment-1/Task-2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/1.EDU/4'th/2'nd/DS/Assignment/Assignment-1/Task-2
+# Install script for directory: C:/Users/hazim/OneDrive/Documents/GitHub/DataStructure/Assignment-1/Task-2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/1.EDU/4'th/2'nd/DS/Assignment/Assignment-1/Task-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/hazim/OneDrive/Documents/GitHub/DataStructure/Assignment-1/Task-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Assignment-1/Task-2/main.cpp
+++ Assignment-1/Task-2/main.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 #define endl '\n';
+template <typename T>
+void printArray(deque<T> dq, int n)
+{
+    for (int i=0; i < n; i++)
+        cout << dq[i] << " ";
+}
 
-vector<int> string_to_vector(string &s){
-    vector<int> v;
+deque<int> string_to_vector(string &s){
+    deque<int> v;
     stringstream ss(s);
     int num;
     while(ss >> num){
@@ -13,19 +19,51 @@ vector<int> string_to_vector(string &s){
     return v;
 }
 
+template <typename T>
+void shellSort(deque<T> &dq , int size , int &comparison , int &move){
+    for(int gap = size/2 ; gap > 0 ; gap/=2){
+        for (int i = gap ; i < size ; ++i) {
+            T temp = dq[i];
+            int j = i;
+            for( ;j >= gap && temp < dq[j - gap]; j-=gap){ // 3  4
+                comparison++;
+                dq[j] = dq[j- gap];
+                move++;
 
+            }
+            dq[j] = temp;
+            move++;
+        }
+    }
+}
 int main() {
     fstream myFile;
     myFile.open("arrays.txt",ios::in);
     if(myFile.is_open()){
         string line;
-        vector<int> v;
+        deque<int> v;
+        deque<int> shellTmp;
+        deque<int> selectionTmp;
+        deque<int> mergeTmp;
         while(getline(myFile,line)){
             v = string_to_vector(line);
-            for (int it : v) {
-                cout<<it<<' ';
-            }
+            int size = v[0];
+            v.pop_front();
+            shellTmp = v , selectionTmp = v , mergeTmp = v;
+            int comparison = 0 , move = 0;
+            //cout<<comparison<<' '<< move << endl;
+            /*-----------------Shell sort------------*/
+            cout<<"Original array: ";
+            printArray(v , size);
             cout<<endl;
+            shellSort(shellTmp, size,comparison,move);
+            cout<<"Shell Sort:"<<endl;
+            cout<<" Sorted Array: ";
+            printArray(shellTmp,size);
+            cout<<" Comparisons: "<<comparison<<endl;
+            cout<<" Moves: "<<move<<endl;
+            cout<<endl;
+            /*-----------------merge sort------------*/
         }
         myFile.close();
     }
